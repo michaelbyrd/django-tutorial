ef4acd0e367cce3f84c5009b69abe89e952eec9c
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-07-08 10:18:21 -0400

    comprehensive test for question.was_published_recently()

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -15,6 +15,25 @@ class QuestionMethodTests(TestCase):
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
-        self.assertEqual(future_question.was_published_recently(), False)
+        result = future_question.was_published_recently()
+        self.assertEqual(result, False)
 
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older that 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        result = old_question.was_published_recently()
+        self.assertEqual(result, False)
 
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions whose pub_date
+        is with the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        result = recent_question.was_published_recently()
+        self.assertEqual(result, True)
