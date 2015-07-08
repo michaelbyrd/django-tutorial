7805a2b7ccb3d1d359ef56b48d5fa9884b1db035
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-07-08 15:05:45 -0400

    bugs with grappelli

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace="polls")),
 ]
